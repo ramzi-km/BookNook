f95715680b8a77209c9775a89ba866a3dad7d184
Ramzi K.M <ramzikm3@gmail.com>
2023-03-05 19:41:30 +0530

    searh,filter,sort and pagination done

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -3,38 +3,80 @@ const Product = require("../models/productModel.js");
 const Category = require("../models/categoryModel.js");
 
 module.exports = {
+  //----------------Home------------------//
   getHome: (req, res) => {
     let user = req.session.user;
     res.render("user/home", { user });
   },
+  //---------------xx--------------------//
+
+  //-------------product listing--------------//
   getShop: async (req, res) => {
     let user = req.session.user;
-    try {
-      let categories = await Category.find({unList:false}).lean();
-      let categoryArray=[]
-      categories.forEach(category => {
-        categoryArray.push(category.name)
-      });
-      let products = await Product.find({unList:false,category:{$in:categoryArray}}).lean();
-      let authors = await Product.aggregate([
-        { $group: { _id: "$author"} },
-      ])
+    const category = req.query.category ?? "";
+    const search = req.query.search ?? "";
+    const sort = req.query.sort ?? 0;
+    const price = req.query.price ?? "";
+    let page = Number(req.query.page ?? 0);
+    page = Math.max(page, 0);
 
-      res.render("user/shop", { user, products, categories,authors });
+    try {
+      let categoryFilter = await Category.find({
+        unList: false,
+        name: new RegExp(category, "i"),
+      }).lean();
+      let categoryArray = categoryFilter.map((category) => category.name);
+
+      const findConditions = {
+        unList: false,
+        category: { $in: categoryArray },
+        name: new RegExp(search, "i"),
+      };
+      if (price === "1") {
+        findConditions.price = { $gt: 99, $lt: 501 };
+      } else if (price === "2") {
+        findConditions.price = { $gt: 500, $lt: 1501 };
+      } else if (price === "3") {
+        findConditions.price = { $gt: 1500 };
+      }
+      const products = await Product.find(findConditions)
+        .sort(sort == "0" ? { createdAt: -1 } : { price: sort })
+        .skip(page * 9)
+        .limit(9)
+        .lean();
+      const productCount = await Product.find(findConditions).lean().count();
+
+      let categories = await Category.find({ unList: false }).lean();
+      let pageCount = Math.ceil(productCount / 9);
+      res.render("user/shop", {
+        user,
+        products,
+        categories,
+        category,
+        search,
+        sort,
+        price,
+        page,
+        pageCount,
+      });
     } catch (err) {
       console.log(err);
-      res.send(err)
+      res.send(err);
     }
   },
-  getProductDetails:async(req,res) => {
-    let user = req.session.user;
-    let productId = req.params.id
-    try {
-      let product = await Product.findById(productId)
+  //-------------xx--------------//
 
-      res.render('user/productDetails',{user,product})
+  //--------------product details page----------//
+  getProductDetails: async (req, res) => {
+    let user = req.session.user;
+    let productId = req.params.id;
+    try {
+      let product = await Product.findById(productId);
+
+      res.render("user/productDetails", { user, product });
     } catch (err) {
-      res.send(err)
+      res.send(err);
     }
   },
+  //----------------xx---------------------------//
 };
