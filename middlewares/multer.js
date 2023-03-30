b39fed7ee8d7fdb100e95e40497e9560266c3792
Ramzi K.M <ramzikm3@gmail.com>
2023-03-30 22:01:21 +0530

    trying to fix error

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -13,19 +13,19 @@ let multerStorage = multer.diskStorage({
 
 let uploadImages = multer({
   storage: multerStorage,
-  // fileFilter: (req, file, cb) => {
-  //   if (
-  //     file.mimetype == "image/jpg" ||
-  //     file.mimetype == "image/jpeg" ||
-  //     file.mimetype == "image/png" ||
-  //     file.mimetype == "image/webp"
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg" ||
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/webp"
 
-  //   ) {
-  //     cb(null, true);
-  //   } else {
-  //     return cb(new Error("File type not supported"));
-  //   }
-  // },
+    ) {
+      cb(null, true);
+    } else {
+      return cb(new Error("File type not supported"));
+    }
+  },
 }).fields([
   { name: "mainImage", maxCount: 1 },
   { name: "coverImage", maxCount: 1 },
