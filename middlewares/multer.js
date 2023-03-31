ba17c81ee271f48b049e068faa338228883ab824
Ramzi K.M <ramzikm3@gmail.com>
2023-03-31 11:20:56 +0530

    meh

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -4,7 +4,7 @@ const multer = require("multer");
 
 let multerStorage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "/public/images/productImages");
+    cb(null, "public/images/productImages");
   },
   filename: function (req, file, cb) {
     cb(null, `${file.fieldname}_${Date.now()}_${file.originalname}`);
